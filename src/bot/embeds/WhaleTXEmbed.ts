1f6d178634b0962338808fd3c83c830022e76363
Doug Flynn <DouglassHooker@gmail.com>
2021-11-14 18:17:21 -0500

    Add some prelim buy/sell differentiation

--- src/bot/embeds/WhaleTXEmbed.ts
+++ src/bot/embeds/WhaleTXEmbed.ts
@@ -2,37 +2,66 @@ import {MessageEmbed} from "discord.js";
 import {Emoji} from "../../constants/emoji";
 import {ITransaction} from "../../types/transaction";
 
-export const createWhaleEmbed = (whale: ITransaction) => new MessageEmbed({
-  title: `${Emoji.WHALE} Whale alert`,
-  description: `From: `,
-  fields: [
-    {
-      name: `Amount (${whale.buyCurrency?.symbol})`,
-      value: `${whale.buyAmount?.toLocaleString() || 'N/A'}`,
-      inline: false,
-    },
-    {
-      name: 'Amount (USD)',
-      value: `$${whale.tradeAmount?.toLocaleString() || 'N/A'}`,
-      inline: true,
-    },
-    {
-      name: `Amount (${whale.sellCurrency?.symbol})`,
-      value: `${whale.sellAmount?.toLocaleString() || 'N/A'} BNB`,
-      inline: true,
-    },
-    {
-      name: 'BSCScan Link',
-      value: `https://bscscan.com/tx/${whale.transaction?.hash}`,
-      inline: false,
-    },
-    {
-      name: '⠀',
-      value: `[Problems or requests? Report here](https://github.com/DukeFerdinand/whale-watcher/issues)`,
-      inline: false,
-    },
-  ],
-  footer: {
-    text: `Made with ${Emoji.HEART} by DukeFerdinand`
+type BuySell = {
+  [key in 'buy' | 'sell']: {
+    symbol?: string;
+    amount?: string;
+    usdAmount?: string;
   }
-})
+} & {
+  actionType: 'sell' | 'buy';
+}
+
+// const formatBuySell = (tr: ITransaction): BuySell => {
+//   return {
+//     buy: {
+//       symbol: tr.buyCurrency?.symbol || ''
+//     },
+//     sell: {
+//
+//     }
+//   }
+// }
+
+export const createWhaleEmbed = (whale: ITransaction) => {
+  let {COIN_SYMBOL} = process.env;
+  if (!COIN_SYMBOL) {
+    COIN_SYMBOL = whale.buyCurrency?.symbol || ''
+  }
+
+
+  return new MessageEmbed({
+    title: `${Emoji.WHALE} Whale alert`,
+    description: `<From address in next release>`,
+    fields: [
+      {
+        name: `Amount (${whale.buyCurrency?.symbol})`,
+        value: `${whale.buyAmount?.toLocaleString() || 'N/A'}`,
+        inline: false,
+      },
+      {
+        name: 'Amount (USD)',
+        value: `$${whale.tradeAmount?.toLocaleString() || 'N/A'}`,
+        inline: true,
+      },
+      {
+        name: `Amount (${whale.sellCurrency?.symbol})`,
+        value: `${whale.sellAmount?.toLocaleString() || 'N/A'} BNB`,
+        inline: true,
+      },
+      {
+        name: 'BSCScan Link',
+        value: `https://bscscan.com/tx/${whale.transaction?.hash}`,
+        inline: false,
+      },
+      {
+        name: '⠀',
+        value: `[Problems or requests? Report here](https://github.com/DukeFerdinand/whale-watcher/issues)`,
+        inline: false,
+      },
+    ],
+    footer: {
+      text: `Made with ${Emoji.HEART} by DukeFerdinand`
+    }
+  })
+}
